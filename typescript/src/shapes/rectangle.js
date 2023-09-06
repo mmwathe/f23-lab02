67513df1f89cd05d08c5fd707b244a59eab708d7
mmwathe <mmwathe@andrew.cmu.edu>
2023-09-06 11:53:26 -0400

    ran tsc again to update the js files

--- typescript/src/shapes/rectangle.js
+++ typescript/src/shapes/rectangle.js
@@ -1,13 +1,23 @@
 "use strict";
+// interface Rectangle {
+//     width: number,
+//     height: number,
+//     computeArea: () => number
+// }
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.newRectangle = void 0;
-function newRectangle(width, height) {
-    return {
-        width: width,
-        height: height,
-        computeArea: function () {
+exports.newRectangle = exports.Rectangle = void 0;
+var Rectangle = /** @class */ (function () {
+    function Rectangle(width, height) {
+        this.width = width;
+        this.height = height;
+        this.computeArea = function () {
             return width * height;
-        }
-    };
+        };
+    }
+    return Rectangle;
+}());
+exports.Rectangle = Rectangle;
+function newRectangle(width, height) {
+    return new Rectangle(width, height);
 }
 exports.newRectangle = newRectangle;
